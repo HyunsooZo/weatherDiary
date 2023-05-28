e540542dc98573f475827a245f6d0e2a2127914b
HyunsooZo <bzhs1992@icloud.com>
2023-05-28 18:42:51 +0900

    final commit

--- src/main/java/zerobase/weather/config/SwaggerConfig.java
+++ src/main/java/zerobase/weather/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package zerobase.weather.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
--- src/main/java/zerobase/weather/repository/DateWeatherRepository.java
+++ src/main/java/zerobase/weather/repository/DateWeatherRepository.java
@@ -3,7 +3,6 @@ package zerobase.weather.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zerobase.weather.domain.DateWeather;
-import zerobase.weather.domain.Diary;
 
 import java.time.LocalDate;
 import java.util.List;
